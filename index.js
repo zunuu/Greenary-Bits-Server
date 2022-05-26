671b2157f89232877858b1848262588ee44fc7e3
zunuu <zunayedhossain99@gmail.com>
2022-05-27 03:48:43 +0600

    make admin api done

--- index.js
+++ index.js
@@ -46,6 +46,23 @@ async function run() {
             res.send(users);
         })
 
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const filter = { email: email };
+
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+
+            res.send(result);
+        })
+
+
+
+
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
