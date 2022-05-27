176fe652ff3c54a60e43cde6d0022dc188281540
zunuu <zunayedhossain99@gmail.com>
2022-05-27 06:23:20 +0600

    make admin

--- index.js
+++ index.js
@@ -46,17 +46,34 @@ async function run() {
             res.send(users);
         })
 
-        app.put('/user/admin/:email', async (req, res) => {
+
+
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
 
-            const filter = { email: email };
 
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc);
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
 
-            res.send(result);
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' })
+            }
+
         })
 
 
