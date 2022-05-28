4510c6731a045259ea9f77ee849b2765643417b2
zunuu <zunayedhossain99@gmail.com>
2022-05-28 19:57:18 +0600

    last try for payment

--- index.js
+++ index.js
@@ -91,17 +91,17 @@ async function run() {
 
         })
 
-        // app.post('/create-payment-intent', async (req, res) => {
-        //     const service = req.body;
-        //     const price = service.price;
-        //     const amount = price * 100;
-        //     const paymentIntent = await stripe.paymentIntent.create({
-        //         amount: amount,
-        //         currency: 'usd',
-        //         payment_method_types: ['card']
-        //     });
-        //     res.send({ clientSecret: paymentIntent.client_secret })
-        // })
+        app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+            const service = req.body;
+            const price = service.price;
+            const amount = price*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount : amount,
+              currency: 'usd',
+              payment_method_types:['card']
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+          });
 
 
 
